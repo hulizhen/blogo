ed6a3bd5c016b39fa5ba8eb0071d27a28bfa3068
Lizhen Hu <hulz.dev@gmail.com>
2021-04-30 20:56:52 +0800

    Serve static files

--- cmd/blogo/main.go
+++ cmd/blogo/main.go
@@ -13,7 +13,8 @@ func main() {
 
 	e := gin.Default()
 	e.LoadHTMLGlob("template/*")
-	e.StaticFile("favicon.ico", cfg.Website.FaviconPath)
+	e.Static("/static", "./static")
+	e.StaticFile("/favicon.ico", cfg.Website.FaviconPath)
 	e.GET("/", func(c *gin.Context) {
 		c.HTML(200, "index.html", gin.H{
 			"title":   cfg.Website.Title,
