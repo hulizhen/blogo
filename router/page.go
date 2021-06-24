e7fe1b5ef8be4b88c62a6406f05d09f04cb581d8
Lizhen Hu <hulz.dev@gmail.com>
2021-06-24 20:52:07 +0800

    Update page routes

--- router/page.go
+++ router/page.go
@@ -1,43 +1,42 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func (r *Router) getHome(c *gin.Context) {
-	c.HTML(200, "home", gin.H{
-		"title":       r.config.Website.Title,
-		"description": r.config.Website.Description,
-		"content":     "This is HOME page.",
+	c.HTML(http.StatusOK, "home", gin.H{
+		"title":   r.config.Website.Title,
+		"content": "This is HOME page.",
 	})
 }
 
 func (r *Router) getArchives(c *gin.Context) {
-	c.HTML(200, "archives", gin.H{
-		"title":       r.config.Website.Title,
-		"description": r.config.Website.Description,
-		"content":     "This is ARCHIVES page.",
+	c.HTML(http.StatusOK, "archives", gin.H{
+		"title":   r.config.Website.Title,
+		"content": "This is ARCHIVES page.",
 	})
 }
 
 func (r *Router) getCategories(c *gin.Context) {
-	c.HTML(200, "categories", gin.H{
-		"title":       r.config.Website.Title,
-		"description": r.config.Website.Description,
-		"content":     "This is CATEGORIES page.",
+	c.HTML(http.StatusOK, "categories", gin.H{
+		"title":   r.config.Website.Title,
+		"content": "This is CATEGORIES page.",
 	})
 }
 
 func (r *Router) getTags(c *gin.Context) {
-	c.HTML(200, "tags", gin.H{
-		"title":       r.config.Website.Title,
-		"description": r.config.Website.Description,
-		"content":     "This is TAGS page.",
+	c.HTML(http.StatusOK, "tags", gin.H{
+		"title":   r.config.Website.Title,
+		"content": "This is TAGS page.",
 	})
 }
 
 func (r *Router) getAbout(c *gin.Context) {
-	c.HTML(200, "about", gin.H{
-		"title":       r.config.Website.Title,
-		"description": r.config.Website.Description,
-		"content":     "This is ABOUT page.",
+	c.HTML(http.StatusOK, "about", gin.H{
+		"title":   r.config.Website.Title,
+		"content": "This is ABOUT page.",
 	})
 }
